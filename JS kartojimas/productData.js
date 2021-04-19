e347493c64bbaca41a51f796392f6e64158d963b
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-04-19 18:15:20 +0300

    arrayMethods: arrayMethods.js file added productData.js file added

---
+++ JS kartojimas/productData.js
@@ -0,0 +1,51 @@
+const data = {
+  tax: 21,
+  products: [
+    {
+      id: "685419",
+      title: "Kompiuteris 3000",
+      price: 401.78,
+      count: 12,
+      currencySign: "€",
+      currency: 'EUR'
+    },
+    {
+      id: "6856147",
+      title: "Monitorius 2000",
+      price: 201.88,
+      count: 17,
+      currencySign: "£",
+      currency: 'GBP'
+    },
+    {
+      id: "698196119",
+      title: "Pelytė KB-68",
+      price: 8.99,
+      count: 11,
+      currencySign: "€",
+      currency: 'EUR'
+    },
+    {
+      id: "68651919",
+      title: "Klaviatūra",
+      price: 15.99,
+      count: 13,
+      currencySign: "$",
+      currency: 'USD'
+    },
+  ],
+  neededCurrencies:[
+    {
+      currency:'EUR',
+      currencySign:"€",
+    },
+    {
+      currency:'USD',
+      currencySign:"$",
+    },
+    {
+      currency:'GBP',
+      currencySign:"£",
+    },
+  ]
+}
