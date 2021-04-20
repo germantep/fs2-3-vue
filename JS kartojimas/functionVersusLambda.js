7c32a56c2e5cbd32f534d6a2cf9e25a680159dde
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-04-20 20:09:59 +0300

    function versus lambda: example

--- JS kartojimas/functionVersusLambda.js
+++ JS kartojimas/functionVersusLambda.js
@@ -1,3 +1,4 @@
+// 'use strict';
 const myConsole = require('./myConsole');
 
 /*
@@ -16,4 +17,72 @@ myConsole.log('Lambda išraiškos automatinis grąžinimas rašant vieną komand
   const printSum = (x, y) => console.log(x + y);
   const printSumResult = printSum(3, 5);
   console.log(printSumResult);
-})
+});
+
+/*
+  Daug svarbesnis skirtumas negu patogus užrašymas yra nuorodos <this> priskyrimas.
+    Funkcija nuorodą <this> traktuoja pagal tai kokiam objektui ji yra priskirta.
+      SVARBU: deklaruojant funkcijas nepriskiriant objektui:
+        * funkcika nuorodą <this> prisiskiria pagal tą apliką, kurioje buvo sukurta.
+        * nurodžius 'use strict' pirmoje failo eilutėje, nuoroda <this> bus undefined
+          * class {...} aplinka visuomet vykdoma 'use strict' rėžimu.   
+    Funkcija taip pat gali nurodyti savo nuorodos <this> reikšmę naudojant metodus:
+      * Function.prototype.bind
+      * Function.prototype.call
+      * Function.prototype.apply
+    Daugiau apie šiuos metodus galite paskaityti faile './functionThisBinding.js'
+
+    Lambda išraiška VISADA naudos nuorodos <this> reikšmę pagal tai, kur buvo sukurta.
+    Jeigu ji nėra priskirta prie jokio objekto, tuomet nuoroda <this> bus tuščias objektas
+    Nuorodos <this> pakeisti, neįmanoma. Tai alternatyvu metodui Function.prototype.bind.
+    Todėl praktikoje, kuomet norima pririšti funkcionalumą prie aplinkos kuriame jis yra kuriamas
+    dažniausiai naudojamos lambda išraiškos. 
+*/
+class Teacher {
+  constructor(subjects) {
+    this.subjects = subjects;
+  }
+
+  printSubjects() {
+    console.table(this.subjects)
+  }
+
+  printSubjectsLambda = () => {
+    console.table(this.subjects)
+  }
+
+  tellStudentToSaySubjects(student) {
+    student.doWhatNeeded(this.printSubjects);
+  }
+
+  tellStudentToSaySubjectsLambda() {
+    student.doWhatNeeded(this.printSubjectsLambda);
+  }
+}
+
+class Student {
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+
+  doWhatNeeded(whatToDo) {
+    try {
+      whatToDo();
+    }
+    catch (ex) {
+      console.log('Sorry, i can\'t, be couse:');
+      console.log(ex.message);
+    }
+  }
+}
+
+const mathTeacher = new Teacher(['Algebra', 'Trygonometry', 'Statistics']);
+const student = new Student('Germantas', 'Pauliukaitis');
+mathTeacher.tellStudentToSaySubjects(student);
+mathTeacher.tellStudentToSaySubjectsLambda(student);
+
+
+
+
+
